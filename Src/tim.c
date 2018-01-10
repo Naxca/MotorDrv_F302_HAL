5f7ce05d648c726f540c2438986d1b0edca668e0
naxca <naxca@live.cn>
2018-01-10 10:58:26 +0800

    drive MY-3514C/3515C.

--- Src/tim.c
+++ Src/tim.c
@@ -110,7 +110,7 @@ void MX_TIM2_Init(void)
   }
 
   sConfigOC.OCMode = TIM_OCMODE_PWM1;
-  sConfigOC.Pulse = 48000;
+  sConfigOC.Pulse = 12000;
   sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
   sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
   if (HAL_TIM_PWM_ConfigChannel(&htim2, &sConfigOC, TIM_CHANNEL_1) != HAL_OK)
